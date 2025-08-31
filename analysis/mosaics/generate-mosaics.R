c8b905b5696577e41563f1829eeed404ffefa187
Felipe Carlos <efelipecarlos@gmail.com>
2025-08-31 05:51:22 -0300

    ogh: add custom function to process ogh mosaics

--- analysis/mosaics/generate-mosaics.R
+++ analysis/mosaics/generate-mosaics.R
@@ -1,3 +1,5 @@
+# Sys.setenv("RESTOREPLUS_CUBE_DIR" = "/data/projects/classification-region-3-bytile/data/derived/cube-region-3/")
+
 set.seed(777)
 
 library(sits)
@@ -8,19 +10,20 @@ library(restoreutils)
 #
 processing_context <- "eco 3"
 
+processing_product <- "ogh"
+
 # Local directories
 base_cubes_dir <- restoreutils::project_cubes_dir()
-base_mosaic_dir <- restoreutils::project_mosaics_dir()
-base_classifications_dir <- restoreutils::project_classifications_dir()
+base_mosaic_dir <- restoreutils::project_mosaics_dir() / processing_product
 
 # Dropbox directory
-base_dropbox_dir <- restoreutils::dropbox_dir("mosaic")
+base_dropbox_dir <- restoreutils::dropbox_dir("mosaic") / processing_product
 
 # Bands
-bands <- c("SWIR16", "NIR08", "BLUE")
+bands <- c("SWIR1", "NIR", "BLUE")
 
 # Processing years
-regularization_years <- 2015:2024
+regularization_years <- c(2010)
 
 # Hardware - Multicores
 multicores <- 60
@@ -43,7 +46,7 @@ for (regularization_year in regularization_years) {
   print(regularization_year)
 
   # Define local directories
-  cube_dir <- restoreutils::create_data_dir(base_cubes_dir, regularization_year)
+  cube_dir <- restoreutils::create_data_dir(base_cubes_dir, paste0("ogh/", regularization_year))
   mosaic_dir <- restoreutils::create_data_dir(base_mosaic_dir, regularization_year)
 
   # Define dropbox directory
@@ -54,8 +57,8 @@ for (regularization_year in regularization_years) {
 
   # Load cube
   cube <- sits_cube(
-    source     = "BDC",
-    collection = "LANDSAT-OLI-16D",
+    source     = "OGH",
+    collection = "LANDSAT-GLAD-2M",
     bands      = bands,
     data_dir   = cube_dir
   )
@@ -65,14 +68,16 @@ for (regularization_year in regularization_years) {
     restoreutils::notify(processing_context,
                          paste("generate mosaics > processing", regularization_year))
 
-    tiles <- restoreutils::cube_to_rgb_mosaic(
+    tiles <- restoreclassificationeco3::cube_to_rgb_mosaic(
       cube       = cube,
       output_dir = mosaic_dir,
       roi_file   = eco_region_roi,
       bands      = bands,
       multicores = multicores
     )
+
   }, error = function(e) {
+    print("error here")
     restoreutils::notify(processing_context,
                          "generate mosaics > error to generate tiles!")
   })
