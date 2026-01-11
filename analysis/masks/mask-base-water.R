d52f13dd7db1e262fb151590b9e0de142b486358
Felipe Carlos <efelipecarlos@gmail.com>
2026-01-11 11:01:35 -0300

    masks: add script to generate water mask

---
+++ analysis/masks/mask-base-water.R
@@ -0,0 +1,25 @@
+library(sits)
+library(restoreutils)
+
+#
+# General definitions
+#
+memsize    <- 180
+multicores <- 95
+
+version <- "v2"
+
+year_to_prepare <- 2024:2000
+
+#
+# 1 Generate forest mask
+#
+prepare_prodes(
+  region_id          = 3,
+  years              = year_to_prepare,
+  version            = version,
+  multicores         = multicores,
+  memsize            = memsize,
+  nonforest_mask     = TRUE,
+  nonforest_complete = TRUE
+)
